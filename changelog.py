b121405f21adfe6be57557e3c89ac931ea4aefa9
glebosotov <gleb.osotov@gmail.com>
2023-04-28 16:28:49 +0300

    other: test outputs

--- changelog.py
+++ changelog.py
@@ -11,7 +11,7 @@ def get_commit_list(url, repo, base_commit, token):
     }
 
     url = f'{url}/api/v1/repos/{repo}/commits?sha={base_commit}'
-    print(url)
+    # print(url)
     response = httpx.get(url, headers=headers)
     if response.status_code != 200:
         return []
