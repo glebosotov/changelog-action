a510deec8b17ff63115252ac1e198ae4a2e13f79
glebosotov <gleb.osotov@gmail.com>
2023-05-25 19:18:54 +0500

    feat: better error reporting

--- changelog.py
+++ changelog.py
@@ -14,6 +14,7 @@ def get_commit_list(url, repo, base_commit, token):
     # print(url)
     response = httpx.get(url, headers=headers)
     if response.status_code != 200:
+        print(f'Error while getting commits: {response.status_code}\nURL: {url}\nResponse: {response.json()}')
         return []
     return response.json()
     
